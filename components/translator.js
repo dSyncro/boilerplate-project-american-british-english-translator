81b6230188b36916629932197bf03622ac4bbdba
Domenico Mancini <mancini.domenico02@gmail.com>
2022-12-23 17:02:43 +0100

    Complete challenge

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,129 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
+
+function getReverseDictionary(obj) {
+  return Object.assign(
+    {},
+    ...Object.entries(obj).map(([k, v]) => ({ [v]: k }))
+  );
+}
+
+function capitalizeFirstLetter(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+const americanToBritishDict = { ...americanOnly, ...americanToBritishSpelling };
+const britishToAmericanDict = {
+  ...britishOnly,
+  ...getReverseDictionary(americanToBritishSpelling),
+};
 
 class Translator {
+  AVAILABLE_LOCALES = {
+    AmericanToBritish: "american-to-british",
+    BritishToAmerican: "british-to-american",
+  };
 
+  get availableLocales() {
+    return Object.values(this.AVAILABLE_LOCALES);
+  }
+
+  toBritishEnglish(text) {
+    const translated = this.translate(
+      text,
+      this.AVAILABLE_LOCALES.AmericanToBritish
+    );
+
+    return translated || text;
+  }
+
+  toAmericanEnglish(text) {
+    const translated = this.translate(
+      text,
+      this.AVAILABLE_LOCALES.BritishToAmerican
+    );
+
+    return translated || text;
+  }
+
+  translate(text, locale) {
+    const targetDictionary =
+      locale === this.AVAILABLE_LOCALES.BritishToAmerican
+        ? britishToAmericanDict
+        : americanToBritishDict;
+
+    const targetTitles =
+      locale === this.AVAILABLE_LOCALES.BritishToAmerican
+        ? getReverseDictionary(americanToBritishTitles)
+        : americanToBritishTitles;
+
+    const timeRegex =
+      locale === this.AVAILABLE_LOCALES.BritishToAmerican
+        ? /([1-9]|1[012]).[0-5][0-9]/g
+        : /([1-9]|1[012]):[0-5][0-9]/g;
+
+    const lowerText = text.toLowerCase();
+    const matchesMap = {};
+
+    // Search for titles/honorifics
+    Object.entries(targetTitles).forEach(([k, v]) => {
+      if (!lowerText.includes(k)) return;
+      matchesMap[k] = capitalizeFirstLetter(v);
+    });
+
+    // Filter words with spaces
+    const wordsWithSpace = Object.fromEntries(
+      Object.entries(targetDictionary).filter(([k, v]) => k.includes(" "))
+    );
+
+    // Search for spaced word matches
+    Object.entries(wordsWithSpace).forEach(([k, v]) => {
+      if (!lowerText.includes(k)) return;
+      matchesMap[k] = v;
+    });
+
+    // Search for individual word matches
+    const individualWords = lowerText.match(/(\w+([-'])(\w+)?['-]?(\w+))|\w+/g);
+    individualWords.forEach((word) => {
+      if (targetDictionary[word]) matchesMap[word] = targetDictionary[word];
+    });
+
+    // Search for time matches
+    const matchedTimes = lowerText.match(timeRegex);
+    if (matchedTimes) {
+      matchedTimes.forEach((e) => {
+        if (locale === this.AVAILABLE_LOCALES.AmericanToBritish)
+          return (matchesMap[e] = e.replace(":", "."));
+        return (matchesMap[e] = e.replace(".", ":"));
+      });
+    }
+
+    if (Object.keys(matchesMap).length === 0) return undefined;
+
+    const translation = this.replaceAll(text, matchesMap);
+    const translationWithHighlight = this.replaceAllWithHighlight(
+      text,
+      matchesMap
+    );
+
+    return [translation, translationWithHighlight];
+  }
+
+  replaceAll(text, matchesMap) {
+    const re = new RegExp(Object.keys(matchesMap).join("|"), "gi");
+    return text.replace(re, (matched) => matchesMap[matched.toLowerCase()]);
+  }
+
+  replaceAllWithHighlight(text, matchesMap) {
+    const re = new RegExp(Object.keys(matchesMap).join("|"), "gi");
+    return text.replace(re, (matched) => {
+      return `<span class="highlight">${
+        matchesMap[matched.toLowerCase()]
+      }</span>`;
+    });
+  }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,28 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const { text, locale } = req.body;
+    if (!locale || text === undefined)
+      return res.json({ error: "Required field(s) missing" });
+
+    if (text === "") return res.json({ error: "No text to translate" });
+
+    if (!translator.availableLocales.includes(locale))
+      return res.json({ error: "Invalid value for locale field" });
+
+    const translation =
+      locale === translator.AVAILABLE_LOCALES.AmericanToBritish
+        ? translator.toBritishEnglish(text)
+        : translator.toAmericanEnglish(text);
+
+    if (translation === text || !translation)
+      return res.json({ text, translation: "Everything looks good to me!" });
+
+    return res.json({ text, translation: translation[1] });
+  });
 };
